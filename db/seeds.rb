4365100941f77cad44d7ab23b019a08deca9e2c1
ricky martinez <rickyjmartinez2@gmail.com>
2023-08-18 12:52:17 -0400

    add jobs index action and index in job controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,18 @@
 # Job.create(
-#   company_id: 1, 
-#   title: "Programmer", 
-#   description: "programs things", 
-#   url: "https://careers.insidehighered.com/job/2956989/software-programmer/?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic", 
-#   location: "Los Angeles, CA", 
+#   company_id: 1,
+#   title: "Programmer",
+#   description: "programs things",
+#   url: "https://careers.insidehighered.com/job/2956989/software-programmer/?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic",
+#   location: "Los Angeles, CA",
 #   salary_range: "100_000 - 120_000"
 # )
 
 # Job.create(
-#   company_id: 2, 
-#   title: "CFO", 
-#   description: "bosses people around", 
-#   url: "https://jobs.cedars-sinai.edu/job/los-angeles/associate-cfo-acfo/252/35755422672?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic", 
-#   location: "Los Angeles, CA", 
+#   company_id: 2,
+#   title: "CFO",
+#   description: "bosses people around",
+#   url: "https://jobs.cedars-sinai.edu/job/los-angeles/associate-cfo-acfo/252/35755422672?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic",
+#   location: "Los Angeles, CA",
 #   salary_range: "200_000 - 250_000"
 # )
 
@@ -33,12 +33,12 @@ while i < company.length
   Company.create(
     name: company[i][1],
     logo: company[i][2],
-    description: company[i][3]
+    description: company[i][3],
   )
   i += 1
 end
 
-job = CSV.ready("jobs.csv")
+job = CSV.read("jobs.csv")
 i = 1
 while i < job.length
   Job.create(
@@ -48,7 +48,7 @@ while i < job.length
     url: job[i][4],
     location: job[i][5],
     active: job[i][6],
-    salary_range: job[i][7]
+    salary_range: job[i][7],
   )
   i += 1
 end
