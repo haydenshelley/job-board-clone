30a935b03792a27df873c8a5004bd899a1837379
jcoffee4477 <jacobcoffee@jacobs-air.mynetworksettings.com>
2023-08-21 14:06:36 -0400

    add tag create

--- app/controllers/tag_controller.rb
+++ app/controllers/tag_controller.rb
@@ -1,2 +1,7 @@
 class TagController < ApplicationController
+
+  def show
+    @tag = Tag.find_by(id: params[:id])
+    render :show
+  end
 end
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,9 +1,23 @@
 class TagsController < ApplicationController
+  before_action :authenticate_admin, only: [:create, :update, :destroy]
   def index
     @tags = Tag.all
   end
 
   def show
     @tag = Tag.find_by(id: params[:id])
+  end
+
+  
+  def new
+    @tag = Tag.new
+    render :new
+  end
+
+  def create
+    @tag = Tag.create(
+     name: params[:tag][:name])
+    redirect_to "/tags"
+    
   end
 end
