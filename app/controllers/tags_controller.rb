30ab0032303bdc22a83334fc428ea7470ffac112
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-08-21 11:04:39 -0700

    tag delete route

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,9 +1,16 @@
 class TagsController < ApplicationController
+  before_action :authenticate_admin, only: [:create, :update, :destroy]
+
   def index
     @tags = Tag.all
   end
 
   def show
     @tag = Tag.find_by(id: params[:id])
+  end
+
+  def destroy
+    @tag = Tag.find_by(id: params[:id])
+    redirect_to "/tags", status: :see_other
   end
 end
