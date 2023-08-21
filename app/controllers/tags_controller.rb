cefbacb55f0042ff6e032633cfa16fa2732ae078
ricky martinez <rickyjmartinez2@gmail.com>
2023-08-21 14:02:13 -0400

    add edit tag to tags controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,4 +6,17 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find_by(id: params[:id])
   end
+
+  def edit
+    @tag = Tag.find_by(id: params[:id])
+    render :edit
+  end
+
+  def update
+    @tag = Tag.find_by(id: params[:id])
+    @Tag.update(
+      name: params[:tag][:name],
+    )
+    redirect_to "/tags"
+  end
 end
